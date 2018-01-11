71090241e6b3d9343aa41396cf5e847e12a63f9f
Saradhi Potharaju <saradhi.potharaju@appdynamics.com>
2018-01-11 17:20:59 +0530

    properties file

--- IVRS-APPLICATION/src/main/java/com/ivrs/controllers/IVRSController.java
+++ IVRS-APPLICATION/src/main/java/com/ivrs/controllers/IVRSController.java
@@ -1,10 +1,12 @@
 package com.ivrs.controllers;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.io.FileReader;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +20,20 @@ import com.google.gson.Gson;
 @RestController
 public class IVRSController {
 
+	static final String PROPERTIES_FILE_NAME="ivrs.properties";
+	static final String CONTROLLER_PHONE="controller";
+	static final String JAVA_PHONE="java";
+	static final String DOTNET_PHONE="dotnet";
+	static final String PIN_NUMBER="pinnumber";
+
 	@RequestMapping(value = "/validatePin")
 	@ResponseBody
-	public ResponseEntity<Object> validatePin(@RequestParam("pin") String pin) {
+	public ResponseEntity<Object> validatePin(@RequestParam("pin") String pin) throws Exception{
 
 		pin = pin.trim();
-		if (!StringUtils.isEmpty(pin) && pin.equalsIgnoreCase("125")) {
+		if (!StringUtils.isEmpty(pin) && pin.equalsIgnoreCase(getPropertyValue(PIN_NUMBER))) {
 			System.out.println("pin: " + pin);
-			System.out.println("------------comming is success block");
+			System.out.println("------------coming in success block");
 			return ResponseEntity.status(HttpStatus.OK).body("success");
 
 		} else {
@@ -38,62 +46,86 @@ public class IVRSController {
 	@RequestMapping(value = "/getAgent", method = RequestMethod.GET )
 	@ResponseBody
 	public ResponseEntity<Object> getAgent(@RequestParam("type") String agentType) {
-		 
-		agentType = agentType.trim();
-		System.out.println("agent type ::===" + agentType);
-		if (!StringUtils.isEmpty(agentType) && (agentType.equalsIgnoreCase("1") || agentType.equalsIgnoreCase("2")
-				|| agentType.equalsIgnoreCase("3") || agentType.equalsIgnoreCase("4") || agentType.equalsIgnoreCase("5")
-				|| agentType.equalsIgnoreCase("6"))) {
-			System.out.println("agent type ::===" + agentType);
+            agentType = agentType.trim();
+            System.out.println("agent type ::===" + agentType);
+            if (!StringUtils.isEmpty(agentType) && (agentType.equalsIgnoreCase("1") || agentType.equalsIgnoreCase("2")
+                    || agentType.equalsIgnoreCase("3") || agentType.equalsIgnoreCase("4") || agentType.equalsIgnoreCase("5")
+                    || agentType.equalsIgnoreCase("6"))) {
+                System.out.println("agent type ::===" + agentType);
 
-			String number = null;
-			switch (Integer.parseInt(agentType)) {
+                String number = null;
 
-			case 1:
-				number = getControllerAgent();
-				break;
-			case 2:
-				number = getNetAgent();
-				break;
+                try {
+                    switch (Integer.parseInt(agentType)) {
 
-			case 3:
-				number = getJavaAgent();
-			}
+                        case 1:
+                            number = getControllerAgent();
+                            break;
+                        case 2:
+                            number = getNetAgent();
+                            break;
 
-			String jsonStr = null;
-			Map<String, String> numberMap = new HashMap<String, String>();
-			numberMap.put("number", number);
-			Gson gsonObj = new Gson();
-			jsonStr = gsonObj.toJson(numberMap);
-			System.out.println("Returning json object" + jsonStr);
-			return new ResponseEntity<Object>(jsonStr, HttpStatus.OK);
-			
+                        case 3:
+                            number = getJavaAgent();
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                String jsonStr = null;
+                Map<String, String> numberMap = new HashMap<String, String>();
+                numberMap.put("number", number);
+                Gson gsonObj = new Gson();
+                jsonStr = gsonObj.toJson(numberMap);
+                System.out.println("Returning json object" + jsonStr);
+                return new ResponseEntity<Object>(jsonStr, HttpStatus.OK);
 
-		} else {
-			System.out.println("Bad request");
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-		}
+
+            } else {
+                System.out.println("Bad request");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+        }
+
+
+
+	static String getPropertyValue(String propertyName) throws  IOException
+	{
+        FileReader reader=null;
+        String propertyValue=null;
+		try
+        {
+            reader=new FileReader(PROPERTIES_FILE_NAME);
+            Properties p=new Properties();
+            p.load(reader);
+            propertyValue= p.getProperty(propertyName);
+        }
+        catch(IOException ie)
+        {
+            ie.printStackTrace();
+        }
+        finally
+        {
+            if(reader!=null) reader.close();
+        }
+        return propertyValue;
 
 	}
 
-	private String getControllerAgent() {
-		//Rajendra's number
-		String number = "+911223444";
-		return number;
+
+	private String getControllerAgent() throws IOException {
+		return getPropertyValue(CONTROLLER_PHONE);
 
 	}
 
-	private String getJavaAgent() {
-		//Har
-		String number = "+911234567";
-		return number;
+	private String getJavaAgent() throws IOException{
+        return getPropertyValue(JAVA_PHONE);
 
 	}
 
-	private String getNetAgent() {
-		//Saradhi's number
-		String number = "+911223444";
-		return number;
+	private String getNetAgent() throws IOException{
+        return getPropertyValue(DOTNET_PHONE);
 
 	}
 
